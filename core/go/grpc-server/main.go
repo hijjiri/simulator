a144309d05f370dd9b572872bf48984d08aa100f
hijjiri <stash1107@gmail.com>
2024-06-20 02:38:54 +0900

    Update main.go

--- core/go/grpc-server/main.go
+++ core/go/grpc-server/main.go
@@ -1,27 +1,42 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"os"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	pb "github.com/hijjiri/core/go/grpc-server/example"
 )
+
+const (
+	defaultPort = "50051"
+)
+
+type server struct {
+	pb.UnimplementedExampleServiceServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloResponse{Message: "Hello " + in.GetName()}, nil
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "50051"
+		port = defaultPort
 	}
 
-	lis, err := net.Listen("tcp", ":" + port)
+	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
 	s := grpc.NewServer()
-	// Register your gRPC services here
-
+	pb.RegisterExampleServiceServer(s, &server{})
+	reflection.Register(s)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
