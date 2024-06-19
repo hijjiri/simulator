5cb16b13d3b0dd31eeb957221b4b758d76e37555
hijjiri <stash1107@gmail.com>
2024-06-20 02:10:10 +0900

    一旦、コミット

--- core/go/grpc-server/main.go
+++ core/go/grpc-server/main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-    "log"
-    "net"
-    "os"
+	"log"
+	"net"
+	"os"
 
-    "google.golang.org/grpc"
+	"google.golang.org/grpc"
 )
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "50051" // デフォルトポート
-    }
-    lis, err := net.Listen("tcp", ":"+port)
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
-    s := grpc.NewServer()
-    // gRPCサーバーの登録コードをここに追加
-    log.Printf("server listening at %v", lis.Addr())
-    if err := s.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %v", err)
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "50051"
+	}
+
+	lis, err := net.Listen("tcp", ":" + port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	// Register your gRPC services here
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
