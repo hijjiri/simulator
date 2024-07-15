c297d5bca562d8ce225df123a06c0e4bddf22ca7
hijjiri <stash1107@gmail.com>
2024-07-15 22:30:25 +0900

    Updated

--- core/go/grpc-server/simulator/service/service.go
+++ core/go/grpc-server/simulator/service/service.go
@@ -3,53 +3,55 @@ package simulator
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"math/rand"
 	"sync"
 	"time"
 
+	au "github.com/hijjiri/simulator/core/go/grpc-server/aura"
+	b "github.com/hijjiri/simulator/core/go/grpc-server/battle"
+	d "github.com/hijjiri/simulator/core/go/grpc-server/deck"
+	e "github.com/hijjiri/simulator/core/go/grpc-server/extension"
+	h "github.com/hijjiri/simulator/core/go/grpc-server/hero"
+	lc "github.com/hijjiri/simulator/core/go/grpc-server/landcryptid"
 	pb "github.com/hijjiri/simulator/core/go/grpc-server/simulator"
 	"github.com/hijjiri/simulator/core/go/grpc-server/user"
 )
 
-// 定数とグローバル変数の定義
 const (
-	workerNum = 10  // ワーカーの数
-	queueSize = 100 // リクエストキューのサイズ
+	workerNum = 10
+	queueSize = 100
 )
 
-var requestQueue chan func() // リクエストキューを保持するチャネル
+var requestQueue chan func()
 
-// init関数
 func init() {
-	requestQueue = make(chan func(), queueSize) // リクエストキューの初期化
-	for i := 0; i < workerNum; i++ {            // ワーカーの数だけゴルーチンを起動
-		go worker() // 各ワーカーゴルーチンを起動
+	requestQueue = make(chan func(), queueSize)
+	for i := 0; i < workerNum; i++ {
+		go worker()
 	}
 }
 
-// ワーカーファンクション
 func worker() {
-	for req := range requestQueue { // リクエストキューからリクエストを受け取る
-		req() // リクエストを処理
+	for req := range requestQueue {
+		req()
 	}
 }
 
-// Server構造体の定義
 type Server struct {
 	pb.UnimplementedSimulatorServiceServer
 }
 
-// SimulateBattle関数
 func (s *Server) SimulateBattle(ctx context.Context, in *pb.SimulateRequest) (*pb.SimulateResponse, error) {
-	resultCh := make(chan *pb.SimulateResponse, 1) // 結果を受け取るチャネル
-	errorCh := make(chan error, 1)                 // エラーを受け取るチャネル
+	resultCh := make(chan *pb.SimulateResponse, 1)
+	errorCh := make(chan error, 1)
 
 	var wg sync.WaitGroup
-	wg.Add(1) // ワーカーグループに1つのワーカーを追加
+	wg.Add(1)
 
-	requestQueue <- func() { // リクエストキューにリクエストを追加
-		defer wg.Done() // 関数の終了時にワーカーグループから1つのワーカーを削除
+	requestQueue <- func() {
+		defer wg.Done()
 
 		// ユーザーIDをランダムに生成
 		rand.Seed(time.Now().UnixNano())
@@ -79,6 +81,25 @@ func (s *Server) SimulateBattle(ctx context.Context, in *pb.SimulateRequest) (*p
 		defender := rand.Uint32()
 		resultCounts := make(map[uint32]int32)
 
+		// NewBattleUnitFromDeck
+		offenseUnits, err := createBattleUnits(ctx, convertDeckUnits(in.GetSimulateOffenseDeck().GetUnits()), true)
+		if err != nil {
+			errorCh <- err
+			return
+		}
+
+		defenseUnits, err := createBattleUnits(ctx, convertDeckUnits(in.GetSimulateDefenseDeck().GetUnits()), false)
+		if err != nil {
+			errorCh <- err
+			return
+		}
+
+		units := append(offenseUnits, defenseUnits...)
+		if len(units) < 6 {
+			errorCh <- fmt.Errorf("units number not enough %v", len(units))
+			return
+		}
+
 		for i := 0; i < int(in.GetCounts()); i++ {
 			winner := rand.Uint32()
 			resultCounts[winner]++
@@ -100,12 +121,11 @@ func (s *Server) SimulateBattle(ctx context.Context, in *pb.SimulateRequest) (*p
 
 	// ゴルーチンでワーカーグループの完了を待つ
 	go func() {
-		wg.Wait() // ワーカーグループがすべて完了するのを待つ
+		wg.Wait()
 		close(resultCh)
 		close(errorCh)
 	}()
 
-	// select文で結果またはエラーのいずれかを待つ
 	select {
 	case res := <-resultCh:
 		return res, nil
@@ -113,5 +133,63 @@ func (s *Server) SimulateBattle(ctx context.Context, in *pb.SimulateRequest) (*p
 		return nil, err
 	case <-ctx.Done():
 		return nil, ctx.Err()
+	}
+}
+
+func createBattleUnits(ctx context.Context, deckUnits []*d.DeckUnit, isAttacker bool) ([]*b.Unit, error) {
+	units := make([]*b.Unit, len(deckUnits))
+
+	for i, deckUnit := range deckUnits {
+		position := int32(i)
+		heroData := createHeroData(deckUnit.HeroId)
+		extension1 := createExtensionData(deckUnit.ExtensionIds[0])
+		extension2 := createExtensionData(deckUnit.ExtensionIds[1])
+		cryptidInfo := &lc.CryptidInfo{}
+		auras := []*au.AuraEffect{}
+		contentType := "simulator"
+
+		units[i] = b.NewBattleUnitFromDeck(position, deckUnit, heroData, extension1, extension2, cryptidInfo, auras, deckUnit.SkillOrders, deckUnit.HeroActiveIndex, contentType)
+	}
+
+	return units, nil
+}
+
+func convertDeckUnits(pbUnits []*pb.DeckUnit) []*d.DeckUnit {
+	deckUnits := make([]*d.DeckUnit, len(pbUnits))
+	for i, pbUnit := range pbUnits {
+		deckUnits[i] = &d.DeckUnit{
+			HeroId:          pbUnit.HeroId,
+			ExtensionIds:    pbUnit.ExtensionIds,
+			SkillOrders:     pbUnit.SkillOrders,
+			HeroActiveIndex: pbUnit.HeroActiveIndex,
+		}
+	}
+	return deckUnits
+}
+
+func createHeroData(heroId uint32) *h.HeroData {
+	return &h.HeroData{
+		HeroId: heroId,
+		Param: &h.Param{
+			Hp:  1000,
+			Phy: 100,
+			Int: 100,
+			Agi: 100,
+		},
+		StockedSkillIds: []uint32{1, 2, 3},
+		Passive:         1,
+	}
+}
+
+func createExtensionData(extensionId uint32) *e.ExtensionData {
+	return &e.ExtensionData{
+		ExtensionId: extensionId,
+		Param: &e.Param{
+			Hp:  100,
+			Phy: 10,
+			Int: 10,
+			Agi: 10,
+		},
+		Active: 1,
 	}
 }
